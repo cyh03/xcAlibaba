09f2c326c98ee746fc3af1358a4e678fd63cdd9d
1406717382@qq.com <1406717382@qq.com>
2019-05-06 20:51:17 +0800

    删除多余的注释

--- xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/config/CustomUserAuthenticationConverter.java
+++ xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/config/CustomUserAuthenticationConverter.java
@@ -14,6 +14,7 @@ import java.util.Map;
 
 @Component
 public class CustomUserAuthenticationConverter extends DefaultUserAuthenticationConverter {
+
     @Autowired
     UserDetailsService userDetailsService;
 
