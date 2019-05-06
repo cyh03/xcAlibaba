b8291d023ef6329da8528504545a72dcf3efe47f
1406717382@qq.com <1406717382@qq.com>
2019-05-06 20:47:28 +0800

    删除多余的注释

--- xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/UserDetailsServiceImpl.java
+++ xc-service-ucenter-auth/src/main/java/com/xuecheng/auth/service/UserDetailsServiceImpl.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
