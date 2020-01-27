87c2849ee58595664e8f75f540bb589f26cd78d7
1406717382@qq.com <1406717382@qq.com>
2020-01-27 10:53:02 +0800

    66

--- xc-govern-gateway/src/main/java/com/xuecheng/govern/gateway/GatewayApplication.java
+++ xc-govern-gateway/src/main/java/com/xuecheng/govern/gateway/GatewayApplication.java
@@ -2,10 +2,7 @@ package com.xuecheng.govern.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.filters.ZuulProperties;
 
 /**
  * @author Administrator
@@ -21,14 +18,16 @@ public class GatewayApplication {
     }
 
     /**
+     *
+     *
      * 动态路由
      * 第一点导入包
      * 第二点配置该代码
      * @return
      */
- //   @ConfigurationProperties("zuul")
-  //  @RefreshScope
-  //  public ZuulProperties  zuulProperties(){
+    //   @ConfigurationProperties("zuul")
+    //  @RefreshScope
+    //  public ZuulProperties  zuulProperties(){
     //    return  new ZuulProperties();
 
 
